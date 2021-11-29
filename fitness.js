1972a8501f4cb95074ddfecb555e9a40458c9daf
Obi1Ken0bi <puzikov.artem@gmail.com>
2021-11-29 12:35:44 +0300

    Initial commit

---
+++ fitness.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressHandlebars= require('express-handlebars');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const hbs=expressHandlebars.create({
+  defaultLayout:'main',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) {
+        this._sections = {};
+      }
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+})
+// view engine setup
+
+app.engine('handlebars',hbs.engine)
+app.set('view engine', 'handlebars')
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//Конец настройки
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/index.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+var topnav = document.getElementById("topnav");
+var sticky=topnav.offsetTop;
+var photo=document.getElementById("photos");
+var photoOffset=photo.offsetTop;
+var photoBox=document.getElementById("photoBox");
+var contacts=document.getElementById("contacts");
+var contactsbox=document.getElementById("contactsBox");
+console.log(contacts.offsetTop)
+var contactsOffset=1822 + window.screen.height;
+function myFunction()
+{
+    if(window.pageYOffset>=sticky)
+    {
+        topnav.classList.add("sticky")
+
+    }else{
+        topnav.classList.remove("sticky");
+    }
+    if(window.pageYOffset>=photoOffset)
+    {
+        photoBox.classList.add("active")
+    }
+    else{
+        photoBox.classList.remove("active")
+    }
+    if(window.pageYOffset>=contactsOffset){
+        contactsbox.classList.add("active")
+        photoBox.classList.remove("active")
+    }
+    else
+    {
+        contactsBox.classList.remove("active")
+    }
+
+}
