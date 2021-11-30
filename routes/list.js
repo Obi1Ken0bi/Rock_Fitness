6a8c13d003919cbbd7a4a2436a359ae3fd96728a
Obi1Ken0bi <puzikov.artem@gmail.com>
2021-11-30 23:09:40 +0300

    some frontend features

--- routes/list.js
+++ routes/list.js
@@ -4,7 +4,7 @@ const emploeesController=require('../controllers/emploeesController')
 const roleMiddleware = require("../bin/middleware/roleMiddleware");
 
 router.get('/Admin/create',roleMiddleware('ADMIN'),emploeesController.GetAdminCreate)
-router.post('/Admin/create',emploeesController.PostAdminCreate)
+router.post('/Admin/create',roleMiddleware('ADMIN'),emploeesController.PostAdminCreate)
 router.post('/Admin/edit',emploeesController.PostAdminEdit)
 router.post('/Admin/delete/:id',emploeesController.PostAdminDelete)
 router.get('/employees',emploeesController.Employees)
