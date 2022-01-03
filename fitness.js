0a3a5f1c281c9fd99a1636ca6e2fb58c3b544d0f
Obi1Ken0bi <puzikov.artem@gmail.com>
2022-01-03 15:50:08 +0300

    contract for client done!

--- fitness.js
+++ fitness.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const expressHandlebars = require('express-handlebars')
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const contractRouter = require('./routes/contract')
 const profileRouter=require('./routes/profile')
 const dbConnectMiddleWare = require('./bin/middleware/dbConnect')
 const app = express();
@@ -59,7 +59,7 @@ app.use((req, res,next) => {
     }
     return next()
 })
-app.use('/users', usersRouter);
+app.use('/contract', contractRouter);
 app.use('/myprofile',profileRouter)
 app.use('/list',listRouter)
 app.use('/', indexRouter);
